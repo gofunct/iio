5cdf2347895db82799cedec6d6743ca95f88b132
Coleman Word <coleman@Colemans-MacBook-Pro.local>
2019-01-13 16:27:43 -0700

    inject

--- inject.go
+++ inject.go
@@ -2,9 +2,41 @@
 
 package iio
 
-import "github.com/google/wire"
+import (
+	"github.com/google/wire"
+	"github.com/mattn/go-colorable"
+	"io"
+	"os"
+)
+
+var Provider = wire.NewSet(
+	NewStdIO,
+)
+
+var FlexProvider = wire.NewSet(
+	New,
+)
 
 func Inject() (*Service, error) {
-	wire.Build(Set)
+	wire.Build(Provider)
 	return &Service{}, nil
+}
+
+func New(o, e io.Writer, i io.Reader) *Service {
+	return &Service{
+		InR:  i,
+		OutW: o,
+		ErrW: e,
+	}
+}
+
+// Stdio returns a standard IO object.
+func NewStdIO() *Service {
+	io := &Service{
+		InR:  os.Stdin,
+		OutW: colorable.NewColorableStdout(),
+		ErrW: colorable.NewColorableStderr(),
+	}
+
+	return io
 }
--- wire_gen.go
+++ wire_gen.go
@@ -5,9 +5,45 @@
 
 package iio
 
+import (
+	"github.com/google/wire"
+	"github.com/mattn/go-colorable"
+	"io"
+	"os"
+)
+
 // Injectors from inject.go:
 
-func NewIO() (*Service, error) {
+func Inject() (*Service, error) {
 	service := NewStdIO()
 	return service, nil
+}
+
+// inject.go:
+
+var Provider = wire.NewSet(
+	NewStdIO,
+)
+
+var FlexProvider = wire.NewSet(
+	New,
+)
+
+func New(o, e io.Writer, i io.Reader) *Service {
+	return &Service{
+		InR:  i,
+		OutW: o,
+		ErrW: e,
+	}
+}
+
+// Stdio returns a standard IO object.
+func NewStdIO() *Service {
+	io2 := &Service{
+		InR:  os.Stdin,
+		OutW: colorable.NewColorableStdout(),
+		ErrW: colorable.NewColorableStderr(),
+	}
+
+	return io2
 }
